de48d27c1da0186b879894b1e5fa266d38592806
Florian Körner <contact@florian-koerner.com>
2026-02-01 14:23:45 +0100

    Revert "Change: Use View Transitions API for avatar swap"
    This reverts commit b07948719fcfb06b20fb8467b9bef3375c4dd395.

--- apps/docs/.vitepress/theme/components/HomeImage.vue
+++ apps/docs/.vitepress/theme/components/HomeImage.vue
@@ -58,17 +58,8 @@ function updateAvatar() {
     }
 
     lastAvatarIndex.value = index;
-
-    const update = () => {
-      avatarList.value[index] = nextAvatar.value;
-      nextAvatar.value = createAvatar();
-    };
-
-    if (document.startViewTransition) {
-      document.startViewTransition(update);
-    } else {
-      update();
-    }
+    avatarList.value[index] = nextAvatar.value;
+    nextAvatar.value = createAvatar();
 
     updateAvatar();
   }, 1000);
@@ -89,11 +80,14 @@ onUnmounted(() => clearTimeout(avatarTimeout.value));
       class="col"
       :style="{ order: key * 2 }"
     >
-      <img
-        :src="avatar.src"
-        :style="{ background: avatar.gradient }"
-        class="image"
-      />
+      <Transition>
+        <img
+          :key="avatar.src"
+          :src="avatar.src"
+          :style="{ background: avatar.gradient }"
+          class="image"
+        />
+      </Transition>
     </div>
   </div>
 </template>
@@ -113,7 +107,6 @@ onUnmounted(() => clearTimeout(avatarTimeout.value));
   mask-size: 100% 100%;
   margin: -12px;
   align-content: center;
-  view-transition-name: home-avatars;
 }
 
 .col {
@@ -146,13 +139,21 @@ onUnmounted(() => clearTimeout(avatarTimeout.value));
 
 .image {
   display: block;
+  position: absolute;
+  top: 6px;
+  right: 6px;
+  bottom: 6px;
+  left: 6px;
   border-radius: 10px;
 }
-</style>
 
-<style>
-::view-transition-old(home-avatars),
-::view-transition-new(home-avatars) {
-  animation-duration: 0.75s;
+.v-enter-active,
+.v-leave-active {
+  transition: opacity 0.75s ease;
+}
+
+.v-enter-from,
+.v-leave-to {
+  opacity: 0;
 }
 </style>
