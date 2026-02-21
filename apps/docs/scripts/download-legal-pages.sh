c3be3897c23cc44421a4b7c504abc8f56e784612
Florian Körner <contact@florian-koerner.com>
2026-02-22 00:02:54 +0100

    Fix: ensure curl command fails on error in download script

--- apps/docs/scripts/download-legal-pages.sh
+++ apps/docs/scripts/download-legal-pages.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
+set -e
 DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" &>/dev/null && pwd)"
 
 echo "Download site notice pages"
 
-curl \
+curl --fail \
   -H "Accept: application/vnd.github.raw" \
   -H "Authorization: Bearer ${PAT}"\
   -H "X-GitHub-Api-Version: 2022-11-28" \
