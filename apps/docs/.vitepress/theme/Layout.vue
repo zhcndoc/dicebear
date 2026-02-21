bd8bd176fb50ece5baeedb73b4e7f7b4a997cbd5
Florian Körner <contact@florian-koerner.com>
2026-02-21 22:52:41 +0100

    Add: Sponsors in aside

--- apps/docs/.vitepress/theme/Layout.vue
+++ apps/docs/.vitepress/theme/Layout.vue
@@ -1,10 +1,26 @@
 <script setup lang="ts">
 import DefaultTheme from 'vitepress/theme';
+import { VPDocAsideSponsors } from 'vitepress/theme';
+import { withBase } from 'vitepress';
 import LayoutFooter from './components/layout/LayoutFooter.vue';
 import LayoutSidebarBefore from './components/layout/LayoutSidebarBefore.vue';
 import './styles/main.scss';
 
 const { Layout } = DefaultTheme;
+
+const sponsors = [
+  {
+    tier: 'CDN sponsored by',
+    size: 'medium' as const,
+    items: [
+      {
+        name: 'bunny.net',
+        img: withBase('/sponsors/bunny-dark.svg'),
+        url: 'https://bunny.net/',
+      },
+    ],
+  },
+];
 </script>
 
 <template>
@@ -12,8 +28,19 @@ const { Layout } = DefaultTheme;
     <template #sidebar-nav-before>
       <LayoutSidebarBefore />
     </template>
+    <template #aside-outline-after>
+      <div class="layout-aside-sponsors">
+        <VPDocAsideSponsors :data="sponsors" />
+      </div>
+    </template>
     <template #layout-bottom>
       <LayoutFooter />
     </template>
   </Layout>
 </template>
+
+<style lang="scss">
+.VPDocAsideOutline.has-outline ~ .layout-aside-sponsors {
+  margin-top: 32px;
+}
+</style>
