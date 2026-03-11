a311078bf43fb1137cad075932b1d6437f7c6d54
Florian Körner <contact@florian-koerner.com>
2026-03-11 22:21:39 +0100

    Add: Favicon via RealFaviconGenerator

--- apps/docs/.vitepress/config.ts
+++ apps/docs/.vitepress/config.ts
@@ -66,7 +66,12 @@ export default defineConfig<ThemeOptions>({
   description:
     'DiceBear is a free, open source avatar library and avatar API with 30+ SVG styles. Generate profile pictures and user placeholder images for any project.',
   head: [
-    ['link', { rel: 'icon', href: '/favicon.ico' }],
+    ['link', { rel: 'icon', type: 'image/png', href: '/favicon-96x96.png', sizes: '96x96' }],
+    ['link', { rel: 'icon', type: 'image/svg+xml', href: '/favicon.svg' }],
+    ['link', { rel: 'shortcut icon', href: '/favicon.ico' }],
+    ['link', { rel: 'apple-touch-icon', sizes: '180x180', href: '/apple-touch-icon.png' }],
+    ['meta', { name: 'apple-mobile-web-app-title', content: 'DiceBear' }],
+    ['link', { rel: 'manifest', href: '/site.webmanifest' }],
     ['meta', { property: 'og:site_name', content: 'DiceBear' }],
     ['meta', { property: 'og:type', content: 'website' }],
     ['meta', { name: 'twitter:card', content: 'summary' }],
